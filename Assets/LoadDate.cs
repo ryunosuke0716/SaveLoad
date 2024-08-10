6e9c1c10b9048f424615d2d25d73cb4d46f72b83
ryunosuke0716 <saka.works0716@gmail.com>
2024-08-10 23:31:23 +0900

    24081002

--- Assets/LoadDate.cs
+++ Assets/LoadDate.cs
@@ -6,10 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class LoadDate : MonoBehaviour
 {
+    public Text text;
+
     public void ClickStartButton()
     {
         string name = PlayerPrefs.GetString("Name");
         string number = PlayerPrefs.GetString("Number");
         Debug.Log("名前 " + name + " 点数　" + number);
+
+        text.text = "名前 " + name + " 点数　" + number;
     }
 }
