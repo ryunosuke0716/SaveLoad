cdd9908b5329add65bb9c12874fa99ad2fd2e9e0
ryunosuke0716 <saka.works0716@gmail.com>
2024-08-14 15:01:35 +0900

    24081401

---
+++ Assets/saveload(List_multidata)/RankingListMulti.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankingListMulti : MonoBehaviour
+{
+    public Text ranktext;
+
+    class Info 
+    {
+        public string name;
+        public int age;
+    
+        public Info(string name, int age) 
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+   
+    public void ClickButton()
+    {
+        Info[] src = new Info[]{new Info("Taro", 40), 
+                      new Info("Jiro", 30),
+                      new Info("Saburo", 20)};
+        
+        //src.Add(Info[]("saka",49));
+
+        var list = new List<Info>();
+      
+        // listに要素を追加
+        list.AddRange(src);
+      
+        // listをソート
+        var c = new Comparison<Info>(Compare);
+        list.Sort(c);
+      
+        foreach (Info i in list) 
+        {
+            Debug.Log(i.name +" "+ i.age);
+            ranktext.text = i.name;
+        }
+    }
+    
+    static int Compare(Info a, Info b) 
+    {
+        return a.age - b.age;
+    }
+}
